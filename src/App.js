bca1f2ec1a38aba55e72026438225f1b50f3960d
Sergey Goryachev <spldead@gmail.com>
2018-06-28 00:54:05 +0300

    add primary button component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+
+import PrimaryButton from './components/buttons/primary_button/PrimaryButton';
+
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <h2>
+                    Buttons
+                </h2>
+                <h3>
+                    Primary button
+                </h3>
+                <PrimaryButton
+                    buttonText='Primary button'
+                />
+            </div>
+        );
+    }
 }
 
 export default App;
