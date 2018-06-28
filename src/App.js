d22fcff3620914fa6abb7f3536eb495355fe56b4
splDead <AveSatan@6>
2018-06-28 15:48:00 +0300

    Add text button

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React, {Component} from 'react';
 import BasicHover from './components/HoverEffect/BasicHover';
 import Ripple from './components/Ripple/Ripple';
 import PrimaryButton from './components/buttons/primary_button/PrimaryButton';
+import TextButton from './components/buttons/text_button/TextButton';
 
 import './App.css';
 
@@ -18,10 +19,15 @@ class App extends Component {
                 </h3>
                 <BasicHover>
                     <Ripple>
-                        <PrimaryButton buttonText='Primary button' onClick={() => {
-
-                            console.log('wooh')
-                        }} />
+                        <PrimaryButton buttonText='Primary button' onClick={() => {}} />
+                    </Ripple>
+                </BasicHover>
+                <h3>
+                    Text button
+                </h3>
+                <BasicHover>
+                    <Ripple color='rgba(0, 0, 0, .3)'>
+                        <TextButton buttonText='Text button' onClick={() => {}} style={{color: 'black'}}/>
                     </Ripple>
                 </BasicHover>
             </div>
